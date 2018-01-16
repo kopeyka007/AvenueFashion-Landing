d00e693c788ed83d46fbf3265f1275eaa6cb6223
ANDRII-PC\Andrii <davudiykandy1994@gmail.com>
2018-01-16 14:58:03 +0200

    first-commit

---
+++ js/mesonry.js
@@ -0,0 +1,8 @@
+var elem = document.querySelector('.grid');
+var msnry = new Masonry( elem, {
+    // options
+    itemSelector: '.grid-item',
+    columnWidth: 400
+});
+
+// element argument can be a selector string
